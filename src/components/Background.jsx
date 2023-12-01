c9be581f2ddc55053420ce1184543a38206496b4
Rainer Regan (Exacode) <rainercoc@gmail.com>
2023-12-01 16:18:57 +0700

    Camera Rig

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -7,6 +7,8 @@ const Background = () => {
   return (
     <>
       <Environment preset='sunset' />
+      {/* <ambientLight /> */}
+      {/* <pointLight position={[10,10,0]} intensity={1000} /> */}
       <Sphere scale={[50, 50, 50]} rotation-y={Math.PI / 2}>
         <meshStandardMaterial color="#d6e6ff" side={THREE.BackSide} />
       </Sphere>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,15 @@
-import { Box, Float, Html, OrbitControls, Plane, Scroll, useScroll } from '@react-three/drei'
-import { useFrame, useThree } from '@react-three/fiber'
+import { OrbitControls, useScroll } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
 import React, { useEffect, useRef, useState } from 'react'
 import { DoubleSide } from 'three'
-import ComputerScreen from './ComputerScreen'
+import ComputerScreen from '../models/ComputerScreen'
+import PortfolioScene from '../models/PortfolioScene'
+
+import * as THREE from 'three'
 
 const camPos = {
-  1: [10, 12, 23],
-  2: [10, 20, 1],
+  1: [9.038196272515794, 6.043369277943344, -6.958425178983971],
+  2: [1.3273417196396151, 2.486090371342157, 1.1605403102830851],
   3: [2, 12, 20],
   4: [12, 12, 0],
 }
@@ -14,12 +17,18 @@ const camPos = {
 const Experience = ({ currentStage, setCurrentStage, setCurrentProgress }) => {
   const [currentCamPos, setCurrentCamPos] = useState(camPos[1])
   const scrollData = useScroll()
+  const [currentLook, setCurrentLook] = useState([0, 0, 0])
+  const [currentFocus, setCurrentFocus] = useState(null)
 
   // References
   const planeRef = useRef()
 
-  useFrame(() => {
+  useFrame((state) => {
     const offset = scrollData.offset
+    const cameraPosition = state.camera.position
+    const cameraRotation = state.camera.rotation
+
+    // console.log(cameraPosition);
 
     setCurrentProgress(offset)
 
@@ -48,27 +57,38 @@ const Experience = ({ currentStage, setCurrentStage, setCurrentProgress }) => {
     }
   }, [currentStage])
 
+  useEffect(() => {
+    // console.log(currentFocus.object.parent.position);
+    if (currentFocus?.position?.x === 0 && currentFocus?.position?.y === 0 && currentFocus?.position?.z === 0) {
+      
+    }
+  }, [currentFocus])
+
   return (
     <group>
-      <CameraRig position={currentCamPos} />
+      <CameraRig position={currentCamPos} targetObject={currentFocus?.object} />
+      {/* <OrbitControls /> */}
 
-      <ComputerScreen />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh >
+      {/* <ComputerScreen /> */}
+      <PortfolioScene scale={[1, 1, 1]} setCurrentLook={setCurrentLook} setCurrentFocus={setCurrentFocus} />
       <mesh ref={planeRef} position={[0, 0, 0]} rotation={[Math.PI / 2, 0, 0]} scale={[100, 100, 100]}>
         <planeGeometry />
-        <meshBasicMaterial color="green" side={DoubleSide} />
+        <meshBasicMaterial color="#7da5ff" side={DoubleSide} />
       </mesh>
     </group >
   )
 }
 
-function CameraRig({ position: [x, y, z] }) {
+function CameraRig({ position: [x, y, z], targetObject }) {
   useFrame((state) => {
     state.camera.position.lerp({ x, y, z }, 0.06)
-    state.camera.lookAt(0, 0, 0)
+    var look = new THREE.Vector3(0,0,0)
+    if (targetObject) {
+      if (targetObject?.position?.x === 0 && targetObject?.position?.y === 0 && targetObject?.position?.z === 0) look = targetObject?.parent?.position
+      else look = targetObject?.position
+    } 
+    state.camera.lookAt(look)
+    state.camera.updateProjectionMatrix();
   })
 }
 

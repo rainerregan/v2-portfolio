15724236f0f5d1e8ccd31b107181e4ddcaa015cb
Rainer Regan (Exacode) <rainercoc@gmail.com>
2023-12-01 11:31:52 +0700

    Update computer screen

--- src/components/ComputerScreen.jsx
+++ src/components/ComputerScreen.jsx
@@ -6,10 +6,8 @@ const ComputerScreen = () => {
   const { gl, viewport } = useThree()
 
   return (
-    <Html portal={{ current: gl.domElement.parentNode }} position={[0, 10, 0]} fullscreen castShadow receiveShadow occlude="blending" transform>
-      <div>
-        slkjfjdfslk
-      </div>
+    <Html portal={{ current: gl.domElement.parentNode }} position={[0, 10, 0]} fullscreen castShadow receiveShadow occlude="blending" transform scale={[0.5, 0.5, 0.5]}>
+      <iframe title="embed" width={1920} height={1080} src="https://exacode.io/" frameBorder={0} />
     </Html>
   )
 }
